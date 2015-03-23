a1792b4e3fa4f8d8d611a52967c4623d404d7e3e
Pavel Dotsulenko <paul@live.ru>
2015-03-23 17:12:16 +0200

    Revert "Provide default value for users array"

--- attributes/default.rb
+++ attributes/default.rb
@@ -46,5 +46,3 @@ default['user']['non_unique']         = "false"
 
 default['user']['data_bag_name']        = "users"
 default['user']['user_array_node_attr'] = "users"
-
-default[default['user']['user_array_node_attr']] = []
